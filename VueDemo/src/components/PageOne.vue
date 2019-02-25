b66677446b9c5b4e75b2ade8ca7846ee4525deda
niuNaruto <niuting604@163.com>
2019-02-25 11:25:17 +0800

    Router

---
+++ VueDemo/src/components/PageOne.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="backView">
+oooo
+    </div>
+</template>
+
+<script>
+export default {
+    name:"PageOne"
+}
+</script>
+
+<style scoped>
+.backView{
+    height: 200px;
+    width: 300px;
+    border: 2px;
+    background-color: yellow;
+    border-color: aqua;
+}
+</style>
--- VueDemo/src/router/index.js
+++ VueDemo/src/router/index.js
@@ -1,15 +1,22 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import PageOne from '@/components/PageOne'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',  //把Router的mode修改为history模式,VueRouter默认的模式为HASH模式
   routes: [
     {
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/pageone',
+      name: 'PageOne',
+      component: PageOne
     }
   ]
 })
